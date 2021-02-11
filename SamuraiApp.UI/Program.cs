d9cf0119d5ee9e7fa7256a3d96ff4f67184dd348
DiracSpace <www.ralphlaurenpolo@gmail.com>
2021-02-11 11:50:53 -0600

    Started adding Samurais

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace SamuraiApp.UI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static SamuraiContext _context = new SamuraiContext();
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adding new Samurais");
+            AddSamurais("Jayson", "Kevin", "Jesse");
+            GetSamurais();
+            Console.WriteLine("Press Any Key .. ");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 
+        /// Gets a list of parameters of type string
+        /// iterates the list and inserts every new samurai
+        /// 
+        /// </summary>
+        /// <param name="names"></param>
+        private static void AddSamurais(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name });
+            }
+            _context.SaveChanges();
+        }
+
+        private static void GetSamurais()
+        {
+            var samurais = _context.Samurais
+                .TagWith("ConsoleApp.Program.GetSamurais method")
+                .ToList();
+            Console.WriteLine($"The Samurai count is { samurais.Count }");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine($"{ samurai.Name }");
+            }
         }
     }
 }
